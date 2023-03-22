a0e2a194861b8e0833c56da4d1d02590c63cbc18
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-22 16:24:47 +0700

    int main

--- prakonline/prakonline.cpp
+++ prakonline/prakonline.cpp
@@ -5,14 +5,13 @@ float PersegiPanjang(float p, float 1) {
 	return p * 1;
 }
 
-float lingkaran(float r) {
-	return 3.14 * r * r;
-}
-
 float segitiga(float a, float t) {
 	return 0.5 * a * t;
 }
 
+float lingkaran(float r) {
+	return 3.14 * r * r;
+}
 int main() {
 
 	float panjang, lebar, tinggi, jejari, alas;

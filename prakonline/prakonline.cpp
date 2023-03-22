b186160558336da1108bb8906d5442437010d205
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-22 15:47:11 +0700

    memasukan semua case

--- prakonline/prakonline.cpp
+++ prakonline/prakonline.cpp
@@ -15,13 +15,52 @@ float segitiga(float a, float t) {
 
 int main()
 {
-	float nilai[10];
-	float panjang, lebar, jejari, alas, tinggi;
+	float panjang, lebar, tinggi, jejari, alas;
 	int pilihan;
-	bool menu;
-	menu = true;
-	while (menu == true)
-	{
 
-	}
+	system("cls")
+	do{
+
+			cout << "------------------" << endl;
+			cout << "------M E N U-----" << endl;
+			cout << "------------------" << endl;
+
+			cout << "1. luas persegi panjang" << endl;
+			cout << "2. luas lingkaran" << endl;
+			cout << "3. luas segitiga" << endl;
+			cout << "4. Exit" << endl;
+
+			cout << "\npilihan (1/2/3/4) : ";
+			cin >> pilihan;
+
+			switch (pilihan) {
+			case 1:
+				cout << "masukan panjang =";
+				cin >> panjang;
+				cout << "masukan lebar =";
+				cin >> lebar;
+				cout << "luas persegi panjang =" << PersegiPanjang(panjang, lebar);
+				break;
+
+			case 2:
+				cout << "maukan alas =";
+				cin >> alas;
+				cout << "masukan tinggi =";
+				cin >> tinggi;
+				cout << "luas segitiga =" << segitiga(alas, tinggi);
+				break;
+			case 3:
+				cout << "masukan jari-jari =";
+				cin >> jejari;
+				cout << "luas lingkaran =" << lingkaran(jejari);
+				break;
+
+			case 4:
+
+				break;
+			default:
+				cout << "pilihan anda salah!";
+				break;
+			}
+	} while (pilihan != 4);
 }

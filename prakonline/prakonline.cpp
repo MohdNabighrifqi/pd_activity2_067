8e2957087a60f60ac364300547e6807f0bc64003
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-22 16:53:57 +0700

    while

--- prakonline/prakonline.cpp
+++ prakonline/prakonline.cpp
@@ -1,66 +1,63 @@
 #include <iostream>
 using namespace std;
 
-float PersegiPanjang(float p, float 1) {
-	return p * 1;
+float PersegiPanjang(float p, float l) {
+	return p*l;
 }
 
 float segitiga(float a, float t) {
-	return 0.5 * a * t;
+	return 0.5*a*t;
 }
 
 float lingkaran(float r) {
-	return 3.14 * r * r;
+	return 3.14*r*r;
 }
 int main() {
 
 	float panjang, lebar, tinggi, jejari, alas;
 	int pilihan;
+	do {
+		cout << "------------------" << endl;
+		cout << "------M E N U-----" << endl;
+		cout << "------------------" << endl;
 
+		cout << "1. luas persegi panjang" << endl;
+		cout << "2. luas segitiga" << endl;
+		cout << "3. luas lingkaran" << endl;
+		cout << "4. Exit" << endl;
 
+		cout << "Masukan pilihan (1/2/3/4) : ";
+		cin >> pilihan;
 
+		switch (pilihan) {
+		case 1:
+			cout << "masukan panjang =";
+			cin >> panjang;
+			cout << "masukan lebar =";
+			cin >> lebar;
+			cout << "luas persegi panjang =" << PersegiPanjang(panjang, lebar);
+			break;
 
-			cout << "------------------" << endl;
-			cout << "------M E N U-----" << endl;
-			cout << "------------------" << endl;
+		case 2:
+			cout << "maukan alas =";
+			cin >> alas;
+			cout << "masukan tinggi =";
+			cin >> tinggi;
+			cout << "luas segitiga =" << segitiga(alas, tinggi);
+			break;
 
-			cout << "1. luas persegi panjang" << endl;
-			cout << "2. luas lingkaran" << endl;
-			cout << "3. luas segitiga" << endl;
-			cout << "4. Exit" << endl;
+		case 3:
+			cout << "masukan jari-jari =";
+			cin >> jejari;
+			cout << "luas lingkaran =" << lingkaran(jejari);
+			break;
 
-			cout << "\npilihan (1/2/3/4) : ";
-			cin >> pilihan;
+		case 4:
 
-			switch (pilihan) {
-			case 1:
-				cout << "masukan panjang =";
-				cin >> panjang;
-				cout << "masukan lebar =";
-				cin >> lebar;
-				cout << "luas persegi panjang =" << PersegiPanjang(panjang, lebar);
-				break;
-
-			case 2:
-				cout << "maukan alas =";
-				cin >> alas;
-				cout << "masukan tinggi =";
-				cin >> tinggi;
-				cout << "luas segitiga =" << segitiga(alas, tinggi);
-				break;
-			
-			case 3:
-				cout << "masukan jari-jari =";
-				cin >> jejari;
-				cout << "luas lingkaran =" << lingkaran(jejari);
-				break;
-
-			case 4:
-
-				break;
-			default:
-				cout << "pilihan anda salah!";
-				break;
-			}
-	
+			break;
+		default:
+			cout << "pilihan anda salah!";
+			break;
+		}
+	} while (pilihan != 4);
 }
